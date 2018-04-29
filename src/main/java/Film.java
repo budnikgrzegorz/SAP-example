c916a30fbbb2d2cd9556484f89097cc0c76a7a05
Grzegorz_B <budnikgrzegorz@interia.pl>
2018-04-30 01:36:07 +0200

    add new class

---
+++ src/main/java/Film.java
@@ -0,0 +1,12 @@
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
+public class Film {
+
+    @Id
+    private
+}
